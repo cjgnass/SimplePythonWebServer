b6c038384fd9f5a9e96f5563ceb1b39d5cbdd60c
cjgnass <cjgnass@gmail.com>
2025-09-10 12:21:53 -0400

    Rewrote do_GET to identify what is being requested. If the request is for a file on disk we respond with the requested html, otherwise we respond with Error_Page html.

--- server.py
+++ server.py
@@ -1,42 +1,56 @@
 import datetime
+import os
+
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 class RequestHandler(BaseHTTPRequestHandler):
-    Page = '''
-        <html> 
-        <body>
-        <table>
-        <tr> <td>Header</td> <td>Value</td> </tr>
-        <tr> <td>Date & Time</td> <td>{date_time}</td> </tr>
-        <tr> <td>Client Host</td> <td>{client_host}</td> </tr>
-        <tr> <td>Client Port</td> <td>{client_port}</td> </tr>
-        <tr> <td>Command</td> <td>{command}</td> </tr>
-        <tr> <td>Path</td> <td>{path}</td> </tr>
-        </table>
-        </body>
-        </html>
-        '''
+    Page = b"""
+    <html>
+    <body>
+    <h1>Bruh</h1>
+    </body>
+    </html>
+    """
+    Error_Page = b"""
+    <html>
+    <body> 
+    <h1>Error accessing {path}</h1> 
+    <p>{e}</p>
+    </body> 
+    </html>
+    """
 
     def do_GET(self):
-        page = self.create_page()
-        self.send_page(page)
+        try:
+            full_path = os.getcwd() + self.path
 
-    def create_page(self):
-        values = {
-            'date_time': datetime.datetime.now(),
-            'client_host': self.client_address[0],
-            'client_port': self.client_address[1],
-            'command': self.command,
-            'path': self.path,
-        }
-        page = self.Page.format(**values)
-        return page
+            if not os.path.exists(full_path):
+                raise FileNotFoundError("'{0}' not found".format(self.path))
+            elif os.path.isfile(full_path):
+                self.handle_file(full_path)
+            else:
+                raise Exception("Unknown Object '{0}'".format(self.path))
+        except Exception as e:
+            self.handle_error(e)
 
-    def send_page(self, page):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
+    def handle_file(self, full_path):
+        try:
+            with open(full_path, "rb") as f:
+                content = f.read()
+            self.send_content(content)
+        except Exception as e:
+            self.handle_error(e)
+
+
+    def handle_error(self, e):
+        content = self.Error_Page.format(path=self.path, e=e)
+        self.send_content(content, 404)
+
+    def send_content(self, content, status_code=200):
+        self.send_response(status_code)
+        self.send_header('Content-Type', 'text/html')
         self.end_headers()
-        self.wfile.write(page.encode('utf-8'))
+        self.wfile.write(content)
 
 
 if __name__ == '__main__':
