49b9dd929eca23b3bb58ca9501f9386a924aba1b
cjgnass <cjgnass@gmail.com>
2025-09-10 16:16:25 -0400

    Created case classes to help the RequestHandler

--- server.py
+++ server.py
@@ -3,15 +3,56 @@ import os
 
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
+class case_no_file(object):
+    def test(self, handler):
+        return not os.path.exists(handler.full_path)
+
+    def act(self, handler):
+        raise Exception("File not found '{0}'".format(handler.path))
+
+class case_existing_file(object):
+    def test(self, handler):
+        return os.path.isfile(handler.full_path)
+
+    def act(self, handler):
+        handler.handle_file(handler.full_path)
+
+class case_directory_index_file(object):
+    def index_path(self, handler):
+        return os.path.join(handler.full_path, "index.html")
+
+    def test(self, handler):
+        return os.path.isdir(handler.full_path) and os.path.isfile(self.index_path(handler))
+
+    def act(self, handler):
+        handler.handle_file(self.index_path(handler))
+
+class case_directory_no_index_file(object):
+    def index_path(self, handler):
+        return os.path.join(handler.full_path, "index.html")
+
+    def test(self, handler):
+        return os.path.isdir(handler.full_path) and not os.path.isfile(self.index_path(handler))
+
+    def act(self, handler):
+        handler.list_dir(handler.full_path)
+
+class case_fail(object):
+    def test(self, handler):
+        return True
+    def act(self, handler):
+        raise Exception("Unknown Object '{0}'".format(handler.path))
+
 class RequestHandler(BaseHTTPRequestHandler):
-    Page = b"""
-    <html>
+    Listing_Page = """ 
+    <html> 
     <body>
-    <h1>Bruh</h1>
+    <h1>Listing Page</h1>
+    <ul>{0}</ul> 
     </body>
     </html>
     """
-    Error_Page = b"""
+    Error_Page = """
     <html>
     <body> 
     <h1>Error accessing {path}</h1> 
@@ -19,17 +60,17 @@ class RequestHandler(BaseHTTPRequestHandler):
     </body> 
     </html>
     """
+    Cases = [case_no_file, case_existing_file, case_directory_index_file, case_directory_no_index_file, case_fail]
 
     def do_GET(self):
         try:
-            full_path = os.getcwd() + self.path
+            self.full_path = os.getcwd() + "/root" +  self.path
+            for case in self.Cases:
+                handler = case()
+                if handler.test(self):
+                    handler.act(self)
+                    break
 
-            if not os.path.exists(full_path):
-                raise FileNotFoundError("'{0}' not found".format(self.path))
-            elif os.path.isfile(full_path):
-                self.handle_file(full_path)
-            else:
-                raise Exception("Unknown Object '{0}'".format(self.path))
         except Exception as e:
             self.handle_error(e)
 
@@ -41,6 +82,15 @@ class RequestHandler(BaseHTTPRequestHandler):
         except Exception as e:
             self.handle_error(e)
 
+    def list_dir(self, full_path):
+        try:
+            entries = os.listdir(full_path)
+            print(full_path)
+            bullets = [f'<li><a href={os.path.basename(full_path)}/{e}>{e}</a></li>' for e in entries if not e.startswith('.')]
+            page = self.Listing_Page.format('\n'.join(bullets))
+            self.send_content(page)
+        except Exception as e:
+            self.handle_error(e)
 
     def handle_error(self, e):
         content = self.Error_Page.format(path=self.path, e=e)
@@ -50,7 +100,10 @@ class RequestHandler(BaseHTTPRequestHandler):
         self.send_response(status_code)
         self.send_header('Content-Type', 'text/html')
         self.end_headers()
-        self.wfile.write(content)
+        try:
+            self.wfile.write(content)
+        except TypeError:
+            self.wfile.write(content.encode('utf-8'))
 
 
 if __name__ == '__main__':
